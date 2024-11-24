81c6a9ea3adc8ebaf5fb1c8b4f9174f0b2bca2f9
Ostamer <110486486+Ostamer@users.noreply.github.com>
2024-11-25 02:49:01 +0500

    Add files via upload

--- src/components/api.js
+++ src/components/api.js
@@ -2,8 +2,12 @@ import axios from 'axios';
 
 export const fetchDataFromAPI = async (mode, selectedOption, inputValue) => {
     try {
-        const response = await axios.get('http://127.0.0.1:8000/lemmatize', {
-            params: { mode, selected_option: selectedOption, inputValue }
+        const response = await axios.get('http://176.114.89.106:8000/lemmatize', {
+            params: { 
+                mode, 
+                selected_option: selectedOption, // Приведение к ожидаемому сервером имени
+                input_value: inputValue // Исправление имени параметра
+            }
         });
         return response.data.lemma;
     } catch (error) {
