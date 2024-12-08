1a62924dcd71f88a8f785b5af0be87494baeffab
Ostamer <110486486+Ostamer@users.noreply.github.com>
2024-12-08 11:58:07 +0500

    Add files via upload

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,20 @@
-// config-overrides.js
+const path = require('path');
+
 module.exports = {
-    webpack: (config) => {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        https: require.resolve('https-browserify'),
-        http: require.resolve('stream-http'),
-      };
-      return config;
-    },
-  };
-  
+  webpack: (config) => {
+    // Добавляем полифиллы для https и http
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      https: require.resolve('https-browserify'),
+      http: require.resolve('stream-http'),
+    };
+
+    // Разрешаем импорт модуля https-browserify из node_modules
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'https-browserify': path.resolve(__dirname, 'node_modules/https-browserify'),
+    };
+
+    return config;
+  },
+};
