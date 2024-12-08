8e1f2aa7f2886687f49ec5aadb0011ad041c0cfb
Ostamer <110486486+Ostamer@users.noreply.github.com>
2024-12-08 14:35:23 +0500

    Add files via upload

--- src/components/api.js
+++ src/components/api.js
@@ -1,19 +1,13 @@
 import axios from 'axios';
-import https from 'https-browserify'; // Локальная версия https
-
-// Создаем экземпляр axios с использованием локального https и игнорированием сертификатов
-const axiosInstance = axios.create({
-  httpsAgent: new https.Agent({ rejectUnauthorized: false })  // Игнорирование сертификатов
-});
 
 export const fetchDataFromAPI = async (mode, selectedOption, inputValue) => {
   try {
-    const response = await axiosInstance.get('https://176.114.89.106/lemmatize', {
-      params: { 
-        mode, 
+    const response = await axios.get('https://176.114.89.106/lemmatize', {
+      params: {
+        mode,
         selected_option: selectedOption,
-        input_value: inputValue 
-      }
+        input_value: inputValue,
+      },
     });
     return response.data.lemma;
   } catch (error) {
