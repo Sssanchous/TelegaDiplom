138b06f873b78f2341f71ca1d17f176b6f6423dc
Ostamer <110486486+Ostamer@users.noreply.github.com>
2024-12-08 12:32:52 +0500

    Add files via upload

--- config-overrides.js
+++ config-overrides.js
@@ -5,18 +5,18 @@ module.exports = {
     // Добавляем полифиллы для https и http
     console.log('Webpack config before modification:', config);  // Лог текущей конфигурации
 
+    // Указываем полифиллы для https и http
     config.resolve.fallback = {
       ...config.resolve.fallback,
       https: require.resolve('https-browserify'),
       http: require.resolve('stream-http'),
     };
 
-    // Если вы хотите использовать https-browserify, указывать путь к нему в src/libs не нужно.
-    // Достаточно использовать require.resolve для указания местоположения пакета.
-    // config.resolve.alias = {
-    //   ...config.resolve.alias,
-    //   'https-browserify': path.resolve(__dirname, 'src/libs/https-browserify'),
-    // };
+    // Создаем алиас для локальной версии https-browserify в src/libs
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'https-browserify': path.resolve(__dirname, 'src/libs/https-browserify'),
+    };
 
     console.log('Webpack config after modification:', config);  // Лог после изменений
 
