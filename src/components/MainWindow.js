46cf3ee193cf3bdeac1525f60c323fab9d3eb72d
Alexandt <dgv17022005@gmail.com>
2025-05-14 15:32:08 +0500

    Revert "update mainwindow.js"
    This reverts commit 17b15aa6dc9f2dbc2d24ee0c9b94fc39d38f6059.

--- src/components/MainWindow.js
+++ src/components/MainWindow.js
@@ -140,7 +140,7 @@ const MainWindow = () => {
     };
 
     return (
-        <div className="bg-gray-100 min-h-[100dvh] w-full flex flex-col px-4 max-w-md mx-auto overflow-y-auto">
+        <div className="bg-gray-100 min-h-screen w-full flex flex-col px-4 max-w-md mx-auto">
             {/* Шапка */}
             <div className="flex justify-between px-5 mt-6">
                 {/* Кнопки вопроса и меню */}
